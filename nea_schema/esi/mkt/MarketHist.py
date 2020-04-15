335a788059892f58c0b855fcc5ee2296b1657bc8
Jason M. Cherry <jcherry@gmail.com>
2020-04-14 17:57:55 -0700

    Chore:Added Market History schema

--- nea_schema/esi/mkt/MarketHist.py
+++ nea_schema/esi/mkt/MarketHist.py
@@ -1,10 +1,12 @@
 from datetime import datetime as dt
 from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.mysql import \
-    DATE as Date, \
-    INTEGER as Integer, \
     BIGINT as BigInt, \
-    DOUBLE as Double
+    DATE as Date, \
+    DOUBLE as Double, \
+    INTEGER as Integer, \
+    TINYTEXT as TinyText
 
 from ... import Base
 
@@ -13,6 +15,7 @@ class MarketHist(Base):
     
     ## Columns
     record_date = Column(Date, primary_key=True, autoincrement=False)
+    etag = Column(TinyText)
     region_id = Column(Integer(unsigned=True), ForeignKey('map_Region.region_id'), primary_key=True, autoincrement=False)
     type_id = Column(Integer(unsigned=True), ForeignKey('inv_Type.type_id'), primary_key=True, autoincrement=False)
     order_count = Column(BigInt(unsigned=True))
@@ -20,6 +23,10 @@ class MarketHist(Base):
     lowest = Column(Double(unsigned=True))
     average = Column(Double(unsigned=True))
     highest = Column(Double(unsigned=True))
+    
+    ## Relationships
+    type = relationship('Type')
+    region = relationship('Region')
 
     @classmethod
     def esi_parse(cls, esi_return):
@@ -28,6 +35,7 @@ class MarketHist(Base):
             cls(**{
                 **data,
                 'record_date': dt.strptime(data.pop('date'), '%Y-%m-%d'),
+                'etag': esi_return.headers.get('Etag'),
             }) for data in data_items
         ]
         return class_obj
