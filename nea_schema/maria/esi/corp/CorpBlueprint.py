fb71a88ed55d3281f39af499693fda207886a346
Jason M. Cherry <jcherry@gmail.com>
2021-04-21 19:36:47 -0700

    Modified CorpBlueprint relationships

--- nea_schema/maria/esi/corp/CorpBlueprint.py
+++ nea_schema/maria/esi/corp/CorpBlueprint.py
@@ -16,9 +16,9 @@ class CorpBlueprint(Base):
     ## Columns
     record_time = Column(DateTime)
     etag = Column(TinyText)
-    item_id = Column(BigInt(unsigned=True), ForeignKey('corp_Asset.item_id'), primary_key=True, autoincrement=False)
+    item_id = Column(BigInt(unsigned=True), primary_key=True, autoincrement=False)
     location_flag = Column(TinyText)
-    location_id = Column(BigInt)
+    location_id = Column(BigInt, ForeignKey('corp_Asset.item_id'))
     material_efficiency = Column(TinyInt(unsigned=True))
     quantity = Column(Integer)
     runs = Column(Integer)
@@ -28,7 +28,8 @@ class CorpBlueprint(Base):
     ## Relationships
     type = relationship('Type')
     blueprint = relationship('Blueprint', viewonly=True)
-    asset = relationship('CorpAsset')
+    #asset = relationship('CorpAsset')
+    location = relationship('CorpAsset')
 
     @classmethod
     def esi_parse(cls, esi_return):
