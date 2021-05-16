c72dc29589baf82e97701d60f0f306b1dcd817a2
Jason M. Cherry <jcherry@gmail.com>
2021-05-15 23:59:01 -0700

    Checkpoint

--- nea_schema/maria/esi/Etag.py
+++ nea_schema/maria/esi/Etag.py
@@ -11,16 +11,16 @@ class Etag(Base):
     __tablename__ = 'esi_Etag'
     
     ## Columns
-    path = Column(VarChar(60), primary_key=True, autoincrement=False)
+    path = Column(VarChar(256), primary_key=True, autoincrement=False)
     last_modified = Column(DateTime)
     etag = Column(TinyText)
     
     @classmethod
     def esi_parse(cls, esi_return, orm=True):
-        record_items = [{
+        record_item = {
             'path': esi_return.url.split('?')[0],
             'last_modified': dt.strptime(esi_return.headers.get('Last-Modified'), '%a, %d %b %Y %H:%M:%S %Z'),
             'etag': esi_return.headers.get('Etag'),
-        }]
-        if orm: record_items = [cls(**row) for row in record_items]
-        return record_items
+        }
+        if orm: record_item = cls(**record_item)
+        return record_item
