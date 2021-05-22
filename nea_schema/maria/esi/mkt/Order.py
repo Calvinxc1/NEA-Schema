6aed309f319b39159d6efbffad4ba42d6ad24d6a
Jason M. Cherry <jcherry@gmail.com>
2021-05-22 11:41:19 -0700

    Bugfixed Orders parser

--- nea_schema/maria/esi/mkt/Order.py
+++ nea_schema/maria/esi/mkt/Order.py
@@ -43,7 +43,7 @@ class Order(Base):
     @classmethod
     def esi_parse(cls, esi_return, orm=True):
         record_time = dt.strptime(esi_return.headers.get('Last-Modified'), '%a, %d %b %Y %H:%M:%S %Z')
-        region_id = int(esi_return.url.split('/')[-1])\
+        region_id = int(esi_return.url.split('?')[0].split('/')[-1])\
             if esi_return.url.startswith('https://esi.evetech.net/latest/markets/structures')\
             else int(esi_return.url.split('/')[-2])
         record_items = [{
